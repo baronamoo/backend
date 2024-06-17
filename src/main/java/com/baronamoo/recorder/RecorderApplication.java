094d2dcdd5294dbe2621962a6ccb4ab3b5fc4040
Huh Woosol <dnthf652@naver.com>
2024-06-17 18:29:38 +0900

    변경사항 1 자바 gui를 이용하여 db의 아이디와 비밀번호를 받아 연결하도록 구현하였습니다.

--- src/main/java/com/baronamoo/recorder/RecorderApplication.java
+++ src/main/java/com/baronamoo/recorder/RecorderApplication.java
@@ -1,13 +1,30 @@
 package com.baronamoo.recorder;
 
+import com.baronamoo.recorder.gui.BaronamooGui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class RecorderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecorderApplication.class, args);
+		BaronamooGui gui = new BaronamooGui();
+
+		while (!gui.isWindowClosed()){
+			try{
+				Thread.sleep(100);
+			}catch (InterruptedException e){
+				e.printStackTrace();
+			}
+        }
+		try{
+			SpringApplication.run(RecorderApplication.class, args);
+		}catch (Exception e){
+			JOptionPane.showMessageDialog(null, "잘못된 입력입니다. 데이터 베이스의 아이디와 비밀번호를 다시 확인해주세요", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+
 	}
 
 }
--- src/main/java/com/baronamoo/recorder/gui/BaronamooGui.java
+++ src/main/java/com/baronamoo/recorder/gui/BaronamooGui.java
@@ -3,20 +3,24 @@ package com.baronamoo.recorder.gui;
 import lombok.Getter;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import static java.lang.System.exit;
 
 @Getter
 public class BaronamooGui extends JFrame {
 
-    private static BaronamooGui instance;
+    private boolean isWindowClosed = false;
 
     public BaronamooGui() {
 
         JPanel panel = new JPanel();
-        JLabel idL = new JLabel("ID : ");
-        JLabel pwL = new JLabel("PASSWORD : ");
+        JLabel idL = new JLabel("DB ID : ");
+        JLabel pwL = new JLabel("DB PASSWORD : ");
         JTextField id = new JTextField(10);
         JPasswordField pw = new JPasswordField(10);
-        JButton loginBtn = new JButton("LOGIN");
+        JButton loginBtn = new JButton("입력");
 
         panel.add(idL);
         panel.add(id);
@@ -31,21 +35,28 @@ public class BaronamooGui extends JFrame {
             System.setProperty("BRONAMOO_DATASOURCE_ID", databaseId);
             System.setProperty("BRONAMOO_DATASOURCE_PASSWORD", databasePwd);
 
-            JOptionPane.showMessageDialog(panel, "Credentials set successfully!");
+            JOptionPane.showMessageDialog(panel, "값이 입력되었습니다");
+            isWindowClosed = true;
+            dispose();
         });
         add(panel);
 
         setVisible(true);	//창을 눈에 보이도록 함
-        setSize(640, 480);	//사이즈는 640*480
+        setSize(540, 180);	//사이즈는 640*480
         setResizable(false);	//사이즈 재조정 불가능
         setLocationRelativeTo(null);	//창이 가운데에 뜨도록 함
-        setDefaultCloseOperation(EXIT_ON_CLOSE);	//창을 끄면 프로그램을 종료
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); // Custom close operation
+
+        // Add a WindowListener to handle window closing event
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                exit(1);
+            }
+        });
     }
 
-    public static synchronized BaronamooGui getInstance() {
-        if (instance == null) {
-            instance = new BaronamooGui();
-        }
-        return instance;
+    public boolean isWindowClosed() {
+        return isWindowClosed;
     }
 }
